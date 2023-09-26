dc3aaecf47cfa3abad1b51ed5e03ea0142f512dc
Spencer Robles <spencerrobles19@gmail.com>
2023-09-26 17:56:37 +0800

    barcode generator app in react native created

---
+++ App.js
@@ -0,0 +1,87 @@
+import { StatusBar } from 'expo-status-bar';
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Not yet scanned');
+
+  const askForCamerPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status == 'granted')
+    })()
+  }
+
+
+  //Request Camera Permission
+  useEffect(() => {
+    askForCamerPermission();
+  }, []);
+
+
+  //What happens when we scan the barcode
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log('Type: ' + type + '\nData ' + data)
+  }
+
+
+  //Check permissions and return the screens 
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for Camera Permission</Text>
+      </View>
+    )
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button title={'Allow Camera'} onPress={() => askForCamerPermission()} />
+      </View>
+    )
+  }
+
+  //Return the View
+  return (
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+      <BarCodeScanner
+      onBarCodeScanned={scanned? undefined: handleBarCodeScanned}
+      style ={{height:400, width:400 }} />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+      {scanned && <Button title={'Scan again'} onPress={()=>setScanned(false)} color='tomato'/>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'tomato'
+  }
+});
